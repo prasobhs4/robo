49de73eaaec1e9cf82f868f80b1d22dfabb1a6dc
prasobhs4 <38857625+prasobhs4@users.noreply.github.com>
2020-04-19 19:45:34 -0500

    Updates

---
+++ precache-manifest.4fda3a3056a4deca30a3ca891a62e20c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27658038da641fa38ebefa6bf0c10e4c",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "a23f03e86f210307b829",
+    "url": "/robo/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f626f9cfb443f410bfa0",
+    "url": "/robo/static/css/main.734ebcce.chunk.css"
+  },
+  {
+    "revision": "a23f03e86f210307b829",
+    "url": "/robo/static/js/2.bc3c70cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.bc3c70cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f626f9cfb443f410bfa0",
+    "url": "/robo/static/js/main.4d0c8130.chunk.js"
+  },
+  {
+    "revision": "c799a2fc9c6a22de8da8",
+    "url": "/robo/static/js/runtime-main.37db6261.js"
+  }
+]);
